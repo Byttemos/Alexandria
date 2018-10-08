3ee02f0e41912a6214a57479d2aebba19568b9d0
Byttemos <hriskaer@gmail.com>
2018-10-08 16:00:44 +0200

    hva fanden

--- Alexandria.py
+++ Alexandria.py
@@ -1,9 +1,15 @@
 # Import af modulet PyPDF2, som bruges til at manipulere PDF dokumenter.
 import PyPDF2
+files = 
+paths = []
+for files in "/home/byttemos/artikler"
+paths.append(files)
+	
 # definition og åbning af dokument som pdfFileObj. Brug odfReader.XXXX til at lege med lortet. Her skal directory laves
 # om til en variabel, så flere PDF'er kan itereres igennem koden. Desuden skal directory virke universelt, i stedet for kun på min maskine.
 pdfFileObj = open('/home/byttemos/Downloads/Biografisk metode.pdf' , 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
 
 # Outputter antallet af sider i PDF'en. Fjern hashtag for at inkludere den linje.
 
