7175bdf9d96af9d7dbc565bee1a91176c602e5d9
Byttemos <hriskaer@gmail.com>
2018-10-09 16:31:07 +0200

    det virker stadig ikke gg

--- Alexandria.py
+++ Alexandria.py
@@ -1,19 +1,18 @@
 # Import af modulet PyPDF2, som bruges til at manipulere PDF dokumenter.
 import PyPDF2
-files = 
-paths = []
-for files in "/home/byttemos/artikler"
-paths.append(files)
-	
+import pathlib
+# definition af path hvorfra PDF'erne skal hentes
+pathlist = path('/home/byttemos/artikler').glob('**/*.pdf')
+for path in pathlist:
+
 # definition og åbning af dokument som pdfFileObj. Brug odfReader.XXXX til at lege med lortet. Her skal directory laves
 # om til en variabel, så flere PDF'er kan itereres igennem koden. Desuden skal directory virke universelt, i stedet for kun på min maskine.
-pdfFileObj = open('/home/byttemos/Downloads/Biografisk metode.pdf' , 'rb')
+pdfFileObj = open(path'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
 
-# Outputter antallet af sider i PDF'en. Fjern hashtag for at inkludere den linje.
-
-# print(pdfReader.numPages)
+# Outputter antallet af sider i PDF'en.
+print(pdfReader.numPages)
 
 
 # Lukker dokumentet igen
